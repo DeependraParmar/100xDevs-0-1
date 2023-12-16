a37ed2d36bba9ea8bb214284a5e696a84ef1cb84
DeependraParmar <deependraparmar1@gmail.com>
2023-12-16 19:03:29 +0530

    Initial Commits

---
+++ 02 JavaScript Foundation/index.js
@@ -0,0 +1,6 @@
+// Que 01. Why we need languages?
+// Languages are basically the way for us to get the work done from the computers and machines. From the top, languages like C++,Java, JavaScript are high level languages which got the compiler to convert the code into machine level code. Then there are low level languages like Assembly language which are directly converted into machine level code. Then there are machine level languages which are directly understood by the machine. After you double click on the application present in your system, the compiler basically compiler the whole set of the files and the package and bring it to the RAM.
+
+// Que. 02 What is a compiler? 
+// Compiler convert high level developer-friendly code into machine level code. It is a program which is used to convert the code into machine level code or 0's and 1's. Compiler of C++ is g++ and for Java is javac.
+
