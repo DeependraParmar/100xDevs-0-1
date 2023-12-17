3d54bf6a1ea2f13ff55f6486ece16d8707405b2b
DeependraParmar <deependraparmar1@gmail.com>
2023-12-17 21:11:30 +0530

    Bash Commands

---
+++ Week_02/2.3_Bash/text.js
@@ -0,0 +1,10 @@
+// basic bash commands 
+// 1. pwd: print working directory
+// 2. cd: change directory
+// 3. ls: list all the files in the current folder
+// 4. mkdir: create or make a new directory
+// 5. touch: used to create an empty file
+// 6. cat: used to read or print the content in the file and can also used to create a file with content.
+// 7. vi editor: vi editor helps to edit the file in three major modes.
+// 8. mv: move a file to another directory
+// 9. cp: copy files
