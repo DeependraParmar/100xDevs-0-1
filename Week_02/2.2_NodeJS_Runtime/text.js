bc9437aaed1d9d2c1c4446bb0233e9a53d00ad07
DeependraParmar <deependraparmar1@gmail.com>
2023-12-17 19:38:30 +0530

    HTTP Server

---
+++ Week_02/2.2_NodeJS_Runtime/text.js
@@ -0,0 +1,47 @@
+// What is EcmaScript?
+// Ecmascript is scripting language basically a specification based on javascript. Ecmascript is standard for Javascript or it is basically the rule book on which javascript work. It also tells how the javascript compilers or the engines should work.
+
+
+// What is Javascript? 
+// JS is a scripting language that conforms to EC. But, there are variou features that are not part of Ecmascript or Javascript. for eg. settimeout() falls under the category of Web APIs. 
+// Common Compilers/Engines:
+// 1. V8 Chromium by Google used in chrome written in C
+// 2. Spidermonkey by Firefox written in C++ and Rust
+
+
+// What is Nodejs?
+// Javascript was meant for browsers so, there is an explicit need to run the javascript in the local machine so, Nodejs is basically a runtime based on Chrome's JS Engine which has the capability to run JS on local machines and use JS for backend development.
+
+
+// What is Bun?
+// Bun is also a Javascript runtime, faster than nodejs. It is written in Zig.
+
+
+
+// What can you do with NodeJS? 
+// 1. Create CLIs
+// 2. Create a Video Player
+// 3. Create a Game
+// 4. Create a HTTP Server
+
+
+// What is a HTTP Server?
+// HTTP is a set of rules and regulations used for the communication between two machines or the computers. More deeply, HTTP helps the frontend and the backend to intercommunicate with the help of data. 
+
+// HTTP server that basically responds to the requests that are made by the clients.
+
+
+// HTTP client components
+// 1. Protocol: http/https: 
+// 2. Address/ url/ip/port
+// 3. Route
+// 4. Headers/ body and the query parameters
+// 5. Method: get,post,put,delete
+
+
+// HTTP server components 
+// 1. Response headers
+// 2. Response body
+// 3. Status Codes
+
+
