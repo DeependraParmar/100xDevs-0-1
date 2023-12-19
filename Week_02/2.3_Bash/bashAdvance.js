77c8a75f7eeada16ea3cf450ec55cdbd66f9dbc8
DeependraParmar <deependraparmar1@gmail.com>
2023-12-19 11:31:45 +0530

    Bash Advance

---
+++ Week_02/2.3_Bash/bashAdvance.js
@@ -0,0 +1,44 @@
+/*
+pwd: print working directory
+ls: print and lists all the files and directories present in the current working directory
+ls -l : lists with all the timestamps, user permissions, owner, etc
+ls -R : prints all the subdirectories along with their contents specified and printed.
+ls -la: prints all along with the hidden files
+ls -s: lists all the files and directories in the order of their size.
+ls zoo*: wildcard search starting with zoo
+
+cat: used to create a file and also read a file
+touch: used to create a new empty file in the pwd
+vi <filename>.extension: use the vim editor to edit the files
+mkdir: used to create a new directory
+rm: used to remove a file
+rm -r directory_name: removes or deleted a directory
+cd: used to change the directory
+cd .. : used to come to just previous directory
+md previousflename newfilename: used to rename a file in the pwd
+md filename directoryname: moves the selected file to the specified directory
+cp file_name directory_name: copies the selected file in the specified directory
+
+
+chmod: used to change the permission of a file or directory for the users, groups or others.
+chmod u+x text.sh: adding a execute permission to text.sh
+chmod u+x -r js_files: adding an exucute permission to js_files directory
+
+echo: used to print a message on the terminal window.
+head file_name: print the head 10 rows of the file
+tail file_name: print the last 10 rows of the file
+head -20 index.js: prints the first 20 lines of code of index.js
+tail -20 index.js: prints the last 20 lines of code of index.js
+wc: used to get no of lines, no of words and no of characters
+grep: used to get no of occurences in a file or some other pattern
+history: prints all the commands that you ran till now.
+
+// creating a bash script
+cat > filename.sh : creates a bash file and put the desired script into it.
+CTRL + D : to save the content
+#!/bin/bash : shebang used to initialise a bash script
+./filename.sh : run the bash script
+
+
+
+*/
