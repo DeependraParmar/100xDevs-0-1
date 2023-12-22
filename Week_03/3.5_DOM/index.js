25cf3498aec475bbf2eb5f3b22d57ba45121f2ff
DeependraParmar <deependraparmar1@gmail.com>
2023-12-22 15:09:32 +0530

    DOM Manipulation

---
+++ Week_03/3.5_DOM/index.js
@@ -0,0 +1,7 @@
+// DOM stands for Document Object Model: It helps us to get, change, add or delete various HTML Elements which plays a major role in making the HTML Page active and more correctly, dynamic.
+/*
+Programming Interface for web documents and is like WEB API for building websites.
+*/
+
+
+// Accessing the document object model(DOM)
