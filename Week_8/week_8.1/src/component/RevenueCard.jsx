2d641ac0ec98b2d146f00e3c5e56e5498d597f75
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-01-20 21:04:01 +0530

    Tailwind and more frontend

---
+++ Week_8/week_8.1/src/component/RevenueCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const RevenueCard = () => {
+  return (
+    <>
+        <div className='bg-white shadow-sm rounded'>
+            <div>{title}</div>
+            ?
+        </div> 
+        <div className='flex justify-between'>
+            <div>
+                ₹ {amount}
+            </div>
+            <div>
+                {}
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default RevenueCard
